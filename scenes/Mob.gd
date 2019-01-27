680aee81cbc37cb0874664924399eee255965002
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2019-01-27 06:16:59 -0300

    Implement mob shoot

--- scenes/Mob.gd
+++ scenes/Mob.gd
@@ -1,11 +1,78 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export var min_speed = 150 # Min speed range
-export var max_speed = 250 # Max speed range
 var mob_types = ["walk", "swim", "fly"]
+export (int) var detect_radius
+export (float) var fire_rate
+export (PackedScene) var Bullet
+var vis_color = Color(.867, .91, .247, 0.1)
+var laser_color = Color(1.0, .329, .298)
+
+var target
+var hit_pos
+var can_shoot = true
 
 func _ready():
+	$AnimatedSprite.self_modulate = Color(0.2, 0, 0)
+	var shape = CircleShape2D.new()
+	shape.radius = detect_radius
+	$Visibility/CollisionShape2D.shape = shape
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$ShootTimer.wait_time = fire_rate
 
-func _on_Visibility_screen_exited():
-	queue_free()
+func _physics_process(delta):
+	update()
+	if target:
+		aim()
+
+func aim():
+	hit_pos = []
+	var space_state = get_world_2d().direct_space_state
+	var target_extents = target.get_node('CollisionShape2D').shape.extents - Vector2(5, 5)
+	var nw = target.position - target_extents
+	var se = target.position + target_extents
+	var ne = target.position + Vector2(target_extents.x, -target_extents.y)
+	var sw = target.position + Vector2(-target_extents.x, target_extents.y)
+	for pos in [target.position, nw, ne, se, sw]:
+		var result = space_state.intersect_ray(position,
+				pos, [self], collision_mask)
+		if result:
+			hit_pos.append(result.position)
+			if result.collider.name == "Player":
+				print("fooooo")
+				$AnimatedSprite.self_modulate.r = 1.0
+				rotation = (target.position - position).angle()
+				if can_shoot:
+					shoot(pos)
+				break
+
+func shoot(pos):
+	var b = Bullet.instance()
+	var a = (pos - global_position).angle()
+	b.start(global_position, a + rand_range(-0.05, 0.05))
+	get_parent().add_child(b)
+	can_shoot = false
+	$ShootTimer.start()
+
+func _draw():
+	draw_circle(Vector2(), detect_radius, vis_color)
+	if target:
+		for hit in hit_pos:
+			draw_circle((hit - position).rotated(-rotation), 5, laser_color)
+			draw_line(Vector2(), (hit - position).rotated(-rotation), laser_color)
+
+func _on_Visibility_body_entered(body):
+	print("lala")
+	if target:
+		return
+	target = body
+
+
+func _on_Visibility_body_exited(body):
+	print("bar")
+	if body == target:
+		target = null
+		$AnimatedSprite.self_modulate.r = 0.2
+
+
+func _on_ShootTimer_timeout():
+	can_shoot = true
